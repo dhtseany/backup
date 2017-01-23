75f20263351f9f44fa656354de2d0caf4f6bf1dc
Sean <ssnell@cmhsol.com>
2017-01-22 19:42:42 -0500

    Added external config file support

--- backup.sh
+++ backup.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
+source /etc/backup.conf
 DATE=$(date)
 DOW=$(date +%u)
 WEEK=$(date +%V)
 MONTH=$(date +"%m")
 YEAR=$(date +"%y")
-
 echo "Backup Set Run on $DATE" >> /var/log/backup.log
 
-function PublicBackup() {
-        EXECUTE=$(tar --listed-incremental=/storage/Backups/Public-$WEEK.snap -I pigz -cf /storage/Backups/Public-$YEAR-$MONTH-$DOW.tar.gz /storage/mounts/Public 3>&1 1>&2 2>&3)
-                if [ $? = 0 ]
-                        then
-                                echo "STATUS=0" >> /var/log/backup.log
-                        else
-                                echo "STATUS=1" >> /var/log/backup.log
-                fi
-}
-PublicBackup
+for i in ${BACKUP_DIR[@]}
+        do
+                JOB_NAME=${i##*/}
+                EXECUTE=$(tar --listed-incremental=$OUTPUT_DIR/$JOB_NAME-$WEEK.snap -I pigz -cf $OUTPUT_DIR/$JOB_NAME-$YEAR-$MONTH-$DOW.tar.gz $i 3>&1 1>&2 2>&3)
+                        if [ $? = 0 ]
+                                then
+                                        echo "Job name $JOB_NAME processed successfully." >> /var/log/backup.log
+                                        echo "STATUS=0" >> /var/log/backup.log
+                                else
+                                        echo "Job name $JOB_NAME failed." >> /var/log/backup.log
+                                        echo "STATUS=1" >> /var/log/backup.log
+                        fi
+done
